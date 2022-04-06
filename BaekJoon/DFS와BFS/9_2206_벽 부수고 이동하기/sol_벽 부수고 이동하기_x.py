680273248667046fb6cfa15542f5631b05a51df8
KWY0218 <wodrk1001@gmail.com>
2022-04-06 23:12:25 +0900

    sol_2206

--- BaekJoon/DFS와BFS/9_2206_벽 부수고 이동하기/sol_벽 부수고 이동하기_x.py
+++ BaekJoon/DFS와BFS/9_2206_벽 부수고 이동하기/sol_벽 부수고 이동하기_x.py
@@ -1,30 +1,33 @@
 import sys
 from collections import deque
 
-def solution(y,x,broken,visited,site):
-    stack = deque([])
-    stack.append((y,x,broken))
-    visited[y][x][broken] = 1
-    
+def solution():
+    move = [(-1,0),(0,1),(1,0),(0,-1)]
+
+    stack = deque([(0,0,0)])
+    visited[0][0][0] = 1
     while stack:
         y,x,broken = stack.popleft()
-        if y==(N-1) and x==(M-1): return visited[y][x][broken]
+        if y == (N-1) and x == (M-1):
+            return visited[N-1][M-1][broken]
         for i in range(4):
             dy = y+move[i][0]
             dx = x+move[i][1]
+            
             if 0<=dy<N and 0<=dx<M:
-                if site[dy][dx] == 0 and visited[dy][dx][broken] == 0:
-                    stack.append((y,x,broken))
-                    visited[dy][dx][broken] = visited[y][x][broken] + 1
-                elif site[dy][dx] == 1 and broken == 1:
-                    stack.append((y,x,broken-1))
-                    visited[dy][dx][broken-1] = visited[y][x][broken] + 1
+                if graph[dy][dx] == 0 and visited[dy][dx][broken] == 0:
+                    visited[dy][dx][broken] = visited[y][x][broken]+1
+                    stack.append((dy,dx,broken))
+                elif graph[dy][dx] == 1 and broken ==0:
+                    visited[dy][dx][broken+1] = visited[y][x][broken]+1
+                    stack.append((dy,dx,broken+1))
     return -1
 
-N, M = map(int,sys.stdin.readline().split())
-site = [list(map(int,sys.stdin.readline().rstrip())) for _ in range(N)]
-move = [(-1,0),(0,1),(1,0),(0,-1)]
+N,M = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().rstrip())) for _ in range(N)]
 visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
-print(visited)
-print(solution(0,0,1,visited,site))
-print(visited)
+print(solution())
+
+'''
+    함수로 만들어서 틀리면 -1을 반환해줘야 함
+'''
