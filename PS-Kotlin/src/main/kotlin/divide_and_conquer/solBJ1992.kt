7b8c7794c5c39c85599b1afbfe14b84214e993ff
KWY <wodrk1001@gmail.com>
2023-01-18 11:25:22 +0900

    sol 1992

--- PS-Kotlin/src/main/kotlin/divide_and_conquer/solBJ1992.kt
+++ PS-Kotlin/src/main/kotlin/divide_and_conquer/solBJ1992.kt
@@ -3,48 +3,51 @@ package divide_and_conquer
 import java.util.*
 
 private lateinit var arr: Array<IntArray>
-private val answer = arrayOf(0, 0)
+private var answer = ""
 
 fun main() = with(Scanner(System.`in`)) {
     val n = readln().toInt()
     arr = Array(n) { IntArray(n) }
-    repeat(n) { idx ->
-        arr[idx] = readln().split(" ").map { it.toInt() }.toIntArray()
+    repeat(n) {
+        // 123456 이런식으로 중간에 공백이 없을 경우 int array 로 바꾸는 방법
+        arr[it] = readln().map { o -> o.toString().toInt() }.toIntArray()
     }
     divide(0, n, 0, n, n)
-    answer.forEach { println(it) }
+    println(answer)
 }
 
 private fun divide(colStart: Int, colEnd: Int, rowStart: Int, rowEnd: Int, size: Int) {
-    // 1칸짜리로 내려왔으면 0 또는 1을 1증가한다
+    // solBJ2630 참조
     if (size == 1) {
-        answer[arr[colStart][rowStart]] += 1
+        answer += arr[colStart][rowStart]
         return
     }
-    // index 내의 값들을 모두 더했을 때
     when (solve(colStart, colEnd, rowStart, rowEnd)) {
-        // 0이라면 모두 0이란 소리니까 0을 증가시킨다.
-        0 -> answer[0] += 1
-        // size*size 라면 모두 1이므로 1을 증가시킨다.
-        size * size -> answer[1] += 1
-        // 그렇지 않으면 4개로 쪼갠다.
+        0 -> {
+            answer += 0
+        }
+        size * size -> {
+            answer += 1
+        }
         else -> {
             val colMid = (colStart + colEnd) / 2
             val rowMid = (rowStart + rowEnd) / 2
+            answer += '('
             divide(colStart, colMid, rowStart, rowMid, size / 2)
             divide(colStart, colMid, rowMid, rowEnd, size / 2)
             divide(colMid, colEnd, rowStart, rowMid, size / 2)
             divide(colMid, colEnd, rowMid, rowEnd, size / 2)
+            answer += ')'
         }
     }
 }
-// index 내의 값들을 모두 더한다.
+
 private fun solve(colStart: Int, colEnd: Int, rowStart: Int, rowEnd: Int): Int {
-    var paper = 0
+    var screen = 0
     for (col in colStart until colEnd) {
         for (row in rowStart until rowEnd) {
-            paper += arr[col][row]
+            screen += arr[col][row]
         }
     }
-    return paper
+    return screen
 }
