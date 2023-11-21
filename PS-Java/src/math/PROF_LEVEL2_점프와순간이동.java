504da2f7ea4046843a143a1b0d422a725cb7a357
KWY <wodrk1001@gmail.com>
2023-11-21 11:45:45 +0900

    점프와순간이동

---
+++ PS-Java/src/math/PROF_LEVEL2_점프와순간이동.java
@@ -0,0 +1,17 @@
+package math;
+
+public class PROF_LEVEL2_점프와순간이동 {
+
+    public class Solution {
+
+        public int solution(int n) {
+            int answer = 0;
+            while (n != 0) {
+                if (n % 2 != 0) answer++;
+                n /= 2;
+            }
+
+            return answer;
+        }
+    }
+}
