8a3e94dd17fdc22df6438a2d13ef6755c2e2698f
KWY <wodrk1001@gmail.com>
2023-12-25 14:52:58 +0900

    잃어버린괄호

--- PS-Java/src/greedy/BJ1541_Silver2_잃어버린괄호.java
+++ PS-Java/src/greedy/BJ1541_Silver2_잃어버린괄호.java
@@ -7,8 +7,29 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
-public class BJ1541 {
+public class BJ1541_Silver2_잃어버린괄호 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String prob = br.readLine();
+
+        String[] nums = prob.split("-");
+        int answer = plusSum(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            answer -= plusSum(nums[i]);
+        }
+        System.out.println(answer);
+    }
+
+    private static int plusSum(String num) {
+        String[] nums = num.split("\\+");
+        int ans = 0;
+        for (String n : nums) {
+            ans += Integer.parseInt(n);
+        }
+        return ans;
+    }
+
+    public static void main2(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
