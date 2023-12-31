92ad1a148d6133458a99ac10f017e78cc0aa29fb
KWY <wodrk1001@gmail.com>
2023-12-31 13:19:13 +0900

    안테나

---
+++ PS-Java/src/greedy/BJ18310_SILVER3_안테나.java
@@ -0,0 +1,22 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ18310_SILVER3_안테나 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        System.out.println(nums[(n - 1) / 2]);
+    }
+}
